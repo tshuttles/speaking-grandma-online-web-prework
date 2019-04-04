87c57e5582e38710ef2c27a5228bb8ed04eda9fc
Tatsuya Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-04 04:34:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  else phrase == "".downcase
+  else phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
