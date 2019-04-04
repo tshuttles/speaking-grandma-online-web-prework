ef938d95025b406fa540db11c97b2a96b8e200f7
Tatsuya Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-04 04:11:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(phrase)
   elsif phrase == "HI GRANDMA".upcase
     puts "NO, NOT SINCE 1938"
   else phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
