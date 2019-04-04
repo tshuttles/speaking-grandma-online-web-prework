bcea8948584e55eeaa52f270bc42f523e2df7013
Tatsuya Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-04 04:14:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,10 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(phrase)
-  if .downcase phrase == "hi grandma"
+def speak_to_grandma(phrase == .downcase)
+  if phrase == "hi grandma"
     return "HUH?! SPEAK UP, SONNY!"
-  elsif .upcase phrase == "HI GRANDMA"
+  elsif phrase == "HI GRANDMA"
     return "NO, NOT SINCE 1938"
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
